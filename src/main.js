67fa786960f0b1967ad85e542eaba2cd1760b170
Jake Warren <jwarshack@gmail.com>
2023-07-17 12:06:28 -0400

    animations

---
+++ src/main.js
@@ -0,0 +1,43 @@
+const button = document.getElementById('btn');
+const modal = document.getElementById('modal');
+const content = modal.querySelector('.content');
+const closeBtn = modal.querySelector('#modal-close');
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelector('h1').classList.replace('hide', 'show');
+  document.querySelector('h1').classList.replace('-translate-x-1/2', '-translate-x-0');
+
+})
+
+showModal = () => {
+  console.log('hey')
+  modal.classList.replace('hide-modal', 'show-modal');
+
+  var show = document.getElementsByClassName('show');
+  for (let i = 0; i < show.length; i++) {
+    console.log(show[i])
+    show[i].classList.replace('show', 'hide');
+  }
+  content.classList.replace('hide-content', 'show-content');
+}
+
+button.addEventListener('click', showModal);
+
+
+closeBtn.addEventListener('click', (event) => {
+  if (modal.classList.contains('show-modal')) {    
+    content.classList.replace('show-content', 'hide-content');
+    modal.classList.replace('show-modal', 'hide-modal');
+    var hide = document.getElementsByClassName('hide');
+    for (let i = 0; i < hide.length; i++) {
+      console.log(hide.length)
+      console.log(hide[i])
+      hide[i].classList.replace('hide', 'show');
+    }
+    
+  }
+
+  // button.addEventListener('click', showModal);
+  
+
+});
+
